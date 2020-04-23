17ec81422e716a5fb1ea4b036105200e7bb6c837
riceCk <876496731@qq.com>
2020-04-23 16:44:12 +0800

    first

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import backend from './utils/backend';
 
 Vue.config.productionTip = false
+
+Object.defineProperty(Vue.prototype, '$backend', {
+  get () {
+    return backend;
+  }
+})
 
 new Vue({
   router,
