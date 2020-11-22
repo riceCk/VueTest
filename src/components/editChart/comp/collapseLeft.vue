7e1457e7d74e98315f249a86ac35035d3ca05959
riceCk <xiehb@tydic.com>
2020-11-22 12:19:56 +0800

    20201122

--- src/components/editChart/comp/collapseLeft.vue
+++ src/components/editChart/comp/collapseLeft.vue
@@ -1,104 +1,111 @@
 <template>
   <div class="chart-db-col-left">
-    <div v-show="activeChart.father !== 'excel'" :id="eChartId" style="width:100%;height:500px;"></div>
+    <div v-show="type.father !== 'excel'" :id="eChartId" style="width:100%;height:500px;"></div>
     <report-table
-      v-show="activeChart.father === 'excel' || formData.showTable"
-      style="width:80%;margin: 0 auto;"
-      :dragId="`${eChartId}-table`"
-      :columns="tableColumn"
-      :props="tablePropData"
+            v-show="type.father === 'excel' || formData.showTable"
+            style="width:80%;margin: 0 auto;"
+            :dragId="`${eChartId}-table`"
+            :columns="tableColumn"
+            :props="tablePropData"
     ></report-table>
   </div>
 </template>
 
 <script>
-  import echarts from "echarts";
-  import reportTable from './reportTable'
+    import echarts from "echarts";
+    import reportTable from './reportTable'
 
-  export default {
-    name: 'collapseLeft',
-    components: {
-      reportTable,
-    },
-    props: {
-      eChartId: {
-        type: String,
-        default: 'eChartId'
-      },
-      optionConfig: {
-        type: Object,
-        default () {
-          return {
-            option: null,
-            type: '',
-            formData: ''
-          }
-        }
-      },
-      tableColumnAndProp: {
-        type: Object,
-      },
-    },
-    data () {
-      return {
-        tableColumn: [],
-        tablePropData: {
-          "header-cell-style": {
-            background: "red",
-            color: "#fff",
-          },
-          data: [],
+    export default {
+        name: 'collapseLeft',
+        components: {
+            reportTable,
         },
-        formData: {},
-        activeChart: {},
-        myChart: ''
-      }
-    },
-    computed: {},
-    watch: {
-      optionConfig: {
-        handler (val) {
-          const {option, type, formData} = val;
-          this.formData = formData
-          this.activeChart = type
-          this.initChart(option)
+        props: {
+            eChartId: {
+                type: String,
+                default: 'eChartId'
+            },
+            optionConfig: {
+                type: Object,
+                default () {
+                    return {
+                        option: null,
+                        type: '',
+                        formData: ''
+                    }
+                }
+            },
+            type: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+            formData: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+            tableColumnAndProp: {
+                type: Object,
+            },
         },
-        deep: true // 对象内部的属性监听，也叫深度监听
-      },
-      tableColumnAndProp: {
-        handler (val) {
-          const {tableColumn, tablePropData} = val
-          this.tableColumn = tableColumn
-          this.tablePropData = tablePropData
+        data () {
+            return {
+                tableColumn: [],
+                tablePropData: {
+                    "header-cell-style": {
+                        background: "red",
+                        color: "#fff",
+                    },
+                    data: [],
+                },
+                myChart: ''
+            }
         },
-        deep: true,
-        immediate: true
-      }
-    },
-    created () {
+        computed: {},
+        watch: {
+            optionConfig: {
+                handler (val) {
+                    this.initChart(val)
+                },
+                deep: true // 对象内部的属性监听，也叫深度监听
+            },
+            tableColumnAndProp: {
+                handler (val) {
+                    const {tableColumn, tablePropData} = val
+                    this.tableColumn = tableColumn
+                    this.tablePropData = tablePropData
+                },
+                deep: true,
+                immediate: true
+            },
+        },
+        created () {
 
-    },
-    mounted () {
+        },
+        mounted () {
 
-    },
-    methods: {
-      // 初始化渲染
-      initChart (option) {
-        if (!this.myChart) {
-          this.myChart = echarts.init(document.getElementById(this.eChartId));
+        },
+        methods: {
+            // 初始化渲染
+            initChart (option) {
+                if (!this.myChart) {
+                    this.myChart = echarts.init(document.getElementById(this.eChartId));
+                }
+                if (option) {
+                    this.renderingDta(option)
+                }
+                window.onresize = () => {
+                    this.myChart.resize()
+                }
+            },
+            // eCharts 图重置
+            renderingDta (option) {
+                this.myChart.clear();
+                this.myChart.setOption(option);
+            },
         }
-        if (option) {
-          this.renderingDta(option)
-        }
-        window.onresize = () => {
-          this.myChart.resize()
-        }
-      },
-      // eCharts 图重置
-      renderingDta (option) {
-        this.myChart.clear();
-        this.myChart.setOption(option);
-      },
     }
-  }
 </script>
--- src/components/editChart/index.vue
+++ src/components/editChart/index.vue
@@ -1,23 +1,25 @@
 <template>
   <!--<el-drawer-->
-    <!--class="chart-edit"-->
-    <!--:with-header="false"-->
-    <!--:visible="dialogVisible"-->
-    <!--size="85%"-->
-    <!--@close="onClose"-->
+  <!--class="chart-edit"-->
+  <!--:with-header="false"-->
+  <!--:visible="dialogVisible"-->
+  <!--size="85%"-->
+  <!--@close="onClose"-->
   <!--&gt;-->
   <div class="chart-edit">
     <section class="chart-wrapper">
       <collapseLeft
-        ref="collapseLeft"
-        :optionConfig="optionConfig"
-        :tableColumnAndProp="tableColumnAndProp">
+          ref="collapseLeft"
+          :optionConfig="optionConfig"
+          :type="tableColumnAndProp.type"
+          :formData="tableColumnAndProp.formData"
+          :tableColumnAndProp="tableColumnAndProp">
       </collapseLeft>
-      <!--<collapseRight-->
-        <!--:tableColumnAndProp="tableColumnAndProp"-->
-        <!--@handleColumnAndData="handleColumnAndData"-->
-        <!--@handleOption="handleOption">-->
-      <!--</collapseRight>-->
+      <collapseRight
+      :tableColumnAndProp="tableColumnAndProp"
+      @handleColumnAndData="handleColumnAndData"
+      @handleCollapseRightConfig="handleCollapseRightConfig">
+      </collapseRight>
     </section>
     <section>
       <el-button type="success" @click="onSave">保存</el-button>
@@ -30,104 +32,117 @@
 </template>
 
 <script>
-  import echarts from "echarts";
-  import collapseRight from './comp/collapseRight'
-  import collapseLeft from './comp/collapseLeft'
-  import {tools} from './utils/tools'
+    import echarts from "echarts";
+    import collapseRight from './comp/collapseRight'
+    import collapseLeft from './comp/collapseLeft'
+    import {tools} from './utils/tools'
 
-  export default {
-    name: 'newEditChart',
-    components: {
-      collapseRight,
-      collapseLeft
-    },
-    props: {
-      dialogVisible: {
-        type: Boolean
-      },
-      tableColumn: {
-        type: Array
-      },
-      tablePropData: {
-        type: Object
-      },
-      type: {
-        type: Object
-      },
-      formData: {
-        type: Object
-      },
-    },
-    data () {
-      return {
-        eChartId: 'eChartId',
-        myChart: '',
-        tableColumnAndProp: {},
-        optionConfig: null,
-      }
-    },
-    computed: {},
-    watch: {},
-    created () {
-
-    },
-    mounted () {
-    },
-    methods: {
-      initData () {
-        const tableColumnAndProp = {}
-        tableColumnAndProp.tableColumn = this.tableColumn
-        tableColumnAndProp.tablePropData = this.tablePropData
-        tableColumnAndProp.type = this.type
-        tableColumnAndProp.formData = this.formData
-        this.tableColumnAndProp = JSON.parse(JSON.stringify(tableColumnAndProp))
-      },
-      // 数据回显
-      /**
-       * {option, type, formData}
-       * @param optionConfig
-       */
-      /**
-       * 优化tableColumnAndProp赋值
-       */
-      handleOption (optionConfig) {
-        // const {option, type, formData} = optionConfig
-        this.tableColumnAndProp.type = type
-        this.tableColumnAndProp.formData = formData
-        this.optionConfig = optionConfig
-      },
-      // 孙级组件传表格参数
-      /**
-       * 优化tableColumnAndProp赋值
-       * @param data
-       */
-      handleColumnAndData (data) {
-        const {tableCols, tableData} = data
-        this.tableColumnAndProp.tableColumn = tableCols.map(item => {
-          return {
-            label: item.txt,
-            prop: item.col
-          }
-        })
-        let tableArrayData = []
-        tableData.forEach(item => {
-          tableArrayData.push(tools.unFlatten(item))
-        })
-        this.tableColumnAndProp.tablePropData.data = tableArrayData
-      },
-      onSave () {
-        this.$emit('handleEditChart', {
-          optionConfig: this.optionConfig,
-          ...this.tableColumnAndProp,
-          image: this.$refs.collapseLeft.myChart.getDataURL({})
-        })
-        this.onClose()
-      },
-      onClose () {
-        this.$emit('update:dialogVisible', false)
-      }
+    export default {
+        name: 'newEditChart',
+        components: {
+            collapseRight,
+            collapseLeft
+        },
+        props: {
+            dialogVisible: {
+                type: Boolean,
+                default: false
+            },
+            tableColumn: {
+                type: Array,
+                default: () => {
+                    return []
+                }
+            },
+            tablePropData: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+            type: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+            formData: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+        },
+        data () {
+            return {
+                eChartId: 'eChartId',
+                myChart: '',
+                tableColumnAndProp: {},
+                optionConfig: null,
+            }
+        },
+        computed: {},
+        watch: {},
+        created () {
+          this.initData()
+        },
+        mounted () {
+        },
+        methods: {
+            initData () {
+                const tableColumnAndProp = {}
+                tableColumnAndProp.tableColumn = this.tableColumn
+                tableColumnAndProp.tablePropData = this.tablePropData
+                tableColumnAndProp.type = this.type
+                tableColumnAndProp.formData = this.formData
+                this.tableColumnAndProp = JSON.parse(JSON.stringify(tableColumnAndProp))
+            },
+            // 数据回显
+            /**
+             * {option, type, formData}
+             * @param optionConfig
+             */
+            /**
+             * 优化tableColumnAndProp赋值
+             */
+            handleCollapseRightConfig (optionConfig) {
+                const {option, type, formData} = optionConfig
+                this.tableColumnAndProp.type = type
+                this.tableColumnAndProp.formData = formData
+                this.optionConfig = option
+            },
+            // 孙级组件传表格参数
+            /**
+             * 优化tableColumnAndProp赋值
+             * @param data
+             */
+            handleColumnAndData (data) {
+                const {tableCols, tableData} = data
+                this.tableColumnAndProp.tableColumn = tableCols.map(item => {
+                    return {
+                        label: item.txt,
+                        prop: item.col
+                    }
+                })
+                let tableArrayData = []
+                tableData.forEach(item => {
+                    tableArrayData.push(tools.unFlatten(item))
+                })
+                this.tableColumnAndProp.tablePropData.data = tableArrayData
+            },
+            onSave () {
+                this.$emit('handleEditChart', {
+                    optionConfig: this.optionConfig,
+                    ...this.tableColumnAndProp,
+                    image: this.$refs.collapseLeft.myChart.getDataURL({})
+                })
+                this.onClose()
+            },
+            onClose () {
+                this.$emit('update:dialogVisible', false)
+            }
+        }
     }
-  }
 </script>
 <style type="text/less" lang="less">
   @import "style/index";
